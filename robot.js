31160facdf89cf46f4668c458239a43c94827d01
GrayEmbrace <frostboy16@gmail.com>
2015-02-14 15:25:09 -0800

    Update robot.js
    Load function moved to robot.js

--- robot.js
+++ robot.js
@@ -1,1 +1,11 @@
 //this class will handle the robot object
+function loadRobot() {
+	var robot = new Sprite();
+	robot.x = 15;
+	robot.y = 500;
+	robot.width = 50;
+	robot.height = 100;
+	robot.image = Textures.load("Pre-Robot.png");
+	world.addChild(robot);
+	return robot;
+}
