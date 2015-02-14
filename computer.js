a0a84a11e3c5bfd934cb007fabe8f15dee6d1dfc
GrayEmbrace <frostboy16@gmail.com>
2015-02-14 15:25:42 -0800

    Update computer.js
    Load function moved to computer.js

--- computer.js
+++ computer.js
@@ -1,2 +1,11 @@
 //this class will handle the computer object.
-
+function loadComputer() {
+	var computer = new Sprite();
+	computer.x = 200;
+	computer.y = 150;
+	computer.width = 400;
+	computer.height = 300;
+	computer.image = Textures.load("comp.png");
+	world.addChild(computer);
+	return computer;
+}
