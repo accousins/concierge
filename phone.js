f8457a4476012e55561d17555b34860324c0b373
GrayEmbrace <frostboy16@gmail.com>
2015-02-14 15:24:22 -0800

    Update phone.js
    Load function moved to phone.js

--- phone.js
+++ phone.js
@@ -1,1 +1,11 @@
 //This class will handle the phone object
+function loadPhone(){
+	var phone = new Sprite();
+	phone.x = 100;
+	phone.y = 500;
+	phone.width = 25;
+	phone.height = 25;
+	phone.image = Textures.load("phone.png");
+	world.addChild(phone);
+	return phone;
+}
