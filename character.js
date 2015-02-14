465a4afc447cad5347a53813a40a35fe9fd0c8a5
GrayEmbrace <frostboy16@gmail.com>
2015-02-14 15:22:19 -0800

    Update character.js
    Load moved to character.js

--- character.js
+++ character.js
@@ -1,1 +1,20 @@
 //this class will handle the main character moving around.
+
+function loadCharacter() {
+	var character = new Sprite();
+	character.x = 200;
+	character.y = 400;
+	character.width = 175;
+	character.height = 200;
+	character.xoffset = -character.width / 2;
+	character.yoffset = -character.height / 2;
+	character.image = Textures.load("MainCharacter.png");
+
+	character.moveTo = function(x, y) {
+		this.x = x;
+		this.y = y;
+	};
+	
+	world.addChild(character);
+	return character;
+}
