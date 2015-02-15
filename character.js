0bdb856cd7840292a6c6f1ce3c2ef82d7d35d4cc
GrayEmbrace <frostboy16@gmail.com>
2015-02-14 18:39:06 -0800

    Update character.js
    Character can now move continuously to clicked locations.

--- character.js
+++ character.js
@@ -2,19 +2,32 @@
 
 function loadCharacter() {
 	var character = new Sprite();
-	character.x = 200;
-	character.y = 400;
+	character.x = 285;
+	character.y = 500;
 	character.width = 175;
 	character.height = 200;
 	character.xoffset = -character.width / 2;
 	character.yoffset = -character.height / 2;
 	character.image = Textures.load("MainCharacter.png");
 
+	character.speed = 0.05;
+	character.destX = character.x;
+	character.destY = character.y;
+
 	character.moveTo = function(x, y) {
-		this.x = x;
-		this.y = y;
+		character.destX = x;
+		character.destY = y;
+		var xd = character.destX - character.x;
+		var yd = character.destY - character.y;
 	};
-	
+
+	character.update = function(d) {
+		var xd = ((this.destX - this.x) * this.speed);
+		var yd = ((this.destY - this.y) * this.speed);
+		this.x += xd;
+		this.y += yd;
+	};
+
 	world.addChild(character);
 	return character;
 }
