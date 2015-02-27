ea2b1fbd1bdc88001692ceab8151a174307ad566
GrayEmbrace <frostboy16@gmail.com>
2015-02-26 19:07:57 -0800

    customers ask questions when clicked on

--- deskQuestion.js
+++ deskQuestion.js
@@ -1,6 +1,6 @@
 function loadDeskQ() {
 	var deskQ = new Sprite();
-	deskQ.x = 625;
+	deskQ.x = 810;
 	deskQ.y = 250;
 	deskQ.width = 150;
 	deskQ.height = 150;
@@ -10,12 +10,18 @@ function loadDeskQ() {
 		var dy = gInput.mouse.y - this.y;
 		if (dy >= 75 && dy <= 99) {
 			console.log("you picked A");
+			deskQ.x = 810;
+			deskQ.y = 250;
 		}
 		if (dy >= 100 && dy <= 124) {
 			console.log("you picked B");
+			deskQ.x = 810;
+			deskQ.y = 250;
 		}
 		if (dy >= 125 && dy <= 150) {
 			console.log("you picked C");
+			deskQ.x = 810;
+			deskQ.y = 250;
 		}
 	};
 
